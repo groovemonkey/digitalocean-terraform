e07cd0ac5fe928f5727d80775b155572ebcb47fe
Dave Cohen <dave@tutorialinux.com>
2018-04-07 22:08:53 +0200

    initial commit -- working project

---
+++ templates.tf
@@ -0,0 +1,8 @@
+data "template_file" "haproxyconf" {
+  template = "${file("${path.module}/config/haproxy.cfg.tpl")}"
+
+  vars {
+    web1_priv_ip = "${digitalocean_droplet.web1.ipv4_address_private}"
+    web2_priv_ip = "${digitalocean_droplet.web2.ipv4_address_private}"
+  }
+}
